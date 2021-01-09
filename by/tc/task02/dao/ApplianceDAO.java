edc8acce86755da9b812144e5c556699bd13cdac
VladislavHarkun <63816951+VladislavHarkun@users.noreply.github.com>
2021-01-09 12:41:43 +0300

    Delete ApplianceDAO.java

--- by/tc/task02/dao/ApplianceDAO.java
+++
@@ -1,8 +0,0 @@
-package by.tc.task02.dao;
-
-import by.tc.task02.entity.Appliance;
-import by.tc.task02.entity.criteria.Criteria;
-
-public interface ApplianceDAO {
-	Appliance find(Criteria criteria);
-}
